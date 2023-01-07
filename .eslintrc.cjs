46df350f4b98d8af9b113f1b9a40655ec1ea34c6
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2023-01-07 09:41:01 +0000

    refactor(eslint): update squonk-eslint config and remove peer deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,10 @@
+// This is a patch so that eslint will load the plugins as dependencies. Otherwise we can to install EVERYTHING in th root project
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
   parserOptions: {
-    project: 'tsconfig.json',
+    project: "tsconfig.json",
     tsconfigRootDir: __dirname,
   },
-  extends: ['@squonk/eslint-config'],
+  extends: ["@squonk/eslint-config"],
 };
