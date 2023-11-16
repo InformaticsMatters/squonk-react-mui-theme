d187a162ce1443697768c834d6a50d50fdeb6c3b
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2023-11-16 22:06:35 +0000

    refactor: remove eslint patch

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
-// This is a patch so that eslint will load the plugins as dependencies. Otherwise we can to install EVERYTHING in th root project
-require("@rushstack/eslint-patch/modern-module-resolution");
-
+/**
+ * @type {import("eslint").Linter.Config}
+ */
 module.exports = {
   parserOptions: {
     project: "tsconfig.json",
