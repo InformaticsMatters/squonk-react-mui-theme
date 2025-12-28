d781934ddd794aa3867bcd30fd357e972011c89b
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2025-12-28 11:29:56 +0000

    ci: refactor release workflow

---
+++ release.config.js
@@ -0,0 +1,29 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+const config = {
+  branches: ["main", { name: "dev", channel: "dev", prerelease: "dev" }],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "pnpm install --no-frozen-lockfile",
+      },
+    ],
+    ["@semantic-release/npm", { npmPublish: true, provenance: true }],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          "chore: release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
+
+export default config;
