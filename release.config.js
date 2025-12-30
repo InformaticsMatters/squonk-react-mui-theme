243933a86a1264ced271443ce9963e6e24206684
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2025-12-30 11:05:48 +0000

    ci: update repository url and semantic release packages

--- release.config.js
+++ release.config.js
@@ -22,7 +22,14 @@ const config = {
           "chore: release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
-    "@semantic-release/github",
+    [
+      "@semantic-release/github",
+      {
+        releasedLabels: ["released"],
+        failComment: false,
+        successComment: false,
+      },
+    ],
   ],
 };
 
