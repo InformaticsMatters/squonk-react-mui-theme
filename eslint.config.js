eaa4c164c5ecaf99acf187b029292d86e34be47e
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2026-05-01 22:00:09 +0100

    chore(eslint): update ESLint config to include ignores for .next directory

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,3 @@
 import config from "@squonk/eslint-config";
 
-export default config
-  .map((c) => ({ ...c, ignores: ["**/dist/**"] }))
-  .slice(0, -1); // Remove the last rule which is for apps
+export default [{ ignores: ["**/dist/**"] }, ...config.slice(0, -1)]; // Remove the last rule which is for apps
